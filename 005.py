65b8921118e57d428911bf9daa32ca51694f174a
Jessica Andrade <105511258+tasjessica@users.noreply.github.com>
2024-12-17 15:07:59 -0300

    Update 005.py

--- 005.py
+++ 005.py
@@ -2,5 +2,5 @@
 
 n = int(input('Digite um numero: '))
 ant = n - 1
-suc = n + 110
+suc = n + 1
 print('O numero que você digitou foi {}. Seu antecessor é {} e seu sucessor é {}' .format(n, ant, suc))
