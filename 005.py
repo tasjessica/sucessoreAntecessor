7e52e90183b987eae33b87e4fb139d94c4f615f1
Jessica Andrade <jessicatandrade@live.com>
2024-12-17 15:07:00 -0300

    first commit

---
+++ 005.py
@@ -0,0 +1,6 @@
+#faça um programa que leia um número inteiro e mostre na tela seu sucessor e antecessor.
+
+n = int(input('Digite um numero: '))
+ant = n - 1
+suc = n + 110
+print('O numero que você digitou foi {}. Seu antecessor é {} e seu sucessor é {}' .format(n, ant, suc))
